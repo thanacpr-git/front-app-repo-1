bb15e4d830dc948eef6bd3769c41bea53ff93913
thanacpr <thanacpr@gmail.com>
2024-11-02 15:04:46 +0000

    Add new blog version 2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 2
+            EKS DEMO Blog version 40
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
